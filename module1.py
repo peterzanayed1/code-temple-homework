318a3b127bc9dc153c8e39c4adae27728f90f18e
peterzanayed1 <30851603+peterzanayed1@users.noreply.github.com>
2023-02-01 16:51:30 -0600

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def SqFt(length,width):
+    return length * width
+
+def circumfrence(radius):
+  return radius * 2 * 3.141592653589793238462643383279502884197
+
